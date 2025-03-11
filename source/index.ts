81a9c86f25792778c2a4c3ea8fd5414a70b0cb6a
Nuelchi <edubem80@gmail.com>
2025-03-11 09:46:14 +0100

    MORE ON Callback

--- source/index.ts
+++ source/index.ts
@@ -25,7 +25,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:9000",
+    origin: process.env.FRONTEND_URL || "https://learnx-official-api.onrender.com/auth/google/",
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"], 
     allowedHeaders: ["Content-Type", "Authorization"], 
