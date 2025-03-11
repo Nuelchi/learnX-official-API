fbea19c235232d87ae4ba0d7ac6eac9656cafa87
Nuelchi <edubem80@gmail.com>
2025-03-11 09:38:20 +0100

    callback fix

--- source/Services/googleAuth.service.ts
+++ source/Services/googleAuth.service.ts
@@ -10,7 +10,7 @@ passport.use(new GoogleStrategy(
     {
         clientID: process.env.GOOGLE_CLIENT_ID!,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        callbackURL: "https://learnx-official-api.onrender.com/auth/google",
+        callbackURL: "https://learnx-official-api.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
         try {
