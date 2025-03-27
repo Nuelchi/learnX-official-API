0d0f2dee37d04fcbf1c7acd838b9a45263672d75
Nuelchi <edubem80@gmail.com>
2025-03-27 22:41:02 +0100

    course update

--- source/Interface/course.interface.ts
+++ source/Interface/course.interface.ts
@@ -2,7 +2,7 @@ export interface Icourse {
   week: Number,
   title: string,
   image: string,
-  category: string,
+  category: "backend" | "frontend" | "product-design" | "data-analysis" | "artificial-intelligence";
   type: "video" | "book"
   weeklyTask: String,
   Link: string,
